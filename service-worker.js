a76d470a9c8cc3b4c7b91dbea9b0d5f6d01f2552
Carlos Mañas <carlos@spacenomads.com>
2021-06-10 21:00:33 +0200

    Add offline mode and remove fontawsome icons

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+'use strict';
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function (event) {
+	event.waitUntil(
+		caches.open('airhorner').then(function (cache) {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/css/app.css',
+				'/css/buttons.css',
+				'/css/core.css',
+				'/css/forms.css',
+				'/css/screens.css',
+				'/img/1925.png',
+				'/img/menu.svg',
+				'/img/menu-disabled.svg',
+				'/img/close.svg',
+				'/img/close-white.svg',
+				'/js/app.js'
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	console.log(event.request.url);
+
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);
+});
+
